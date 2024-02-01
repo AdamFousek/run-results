8910a047571b23c44b0f9c62265a26f4540da764
adamfousek <adamfousekdev@gmail.com>
2024-02-01 08:33:28 +0100

    Add package-lock.json to gitignore since its build on server

--- app/Http/Controllers/Admin/MeasurementController.php
+++ app/Http/Controllers/Admin/MeasurementController.php
@@ -25,12 +25,12 @@ class MeasurementController extends Controller
         $views = MeasurementEvent::selectRaw("COUNT(*) views, attribute page, DATE(created_at) date")
             ->withoutRobots()
             ->groupBy(['date', 'attribute'])
-            ->orderBy('date')
+            ->orderBy('date', 'desc')
             ->get();
         $visitors = MeasurementEvent::selectRaw("COUNT(DISTINCT visitorid) unique_visitors, DATE(created_at) date")
             ->withoutRobots()
             ->groupBy(['date'])
-            ->orderBy('date')
+            ->orderBy('date', 'desc')
             ->get();
 
         $data = [
@@ -39,53 +39,5 @@ class MeasurementController extends Controller
         ];
 
         return Inertia::render('Admin/Measurement/Index', $data);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(MeasurementEvent $measurementEvent)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(MeasurementEvent $measurementEvent)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, MeasurementEvent $measurementEvent)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(MeasurementEvent $measurementEvent)
-    {
-        //
     }
 }
