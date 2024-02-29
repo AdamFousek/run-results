5c67c466e88a3111171d38357a45cd71ffd0c56a
adamfousek <adamfousekdev@gmail.com>
2024-02-29 16:20:54 +0100

    Add log after same last name and year

--- app/Models/Illuminate/Enums/ResultRowEnum.php
+++ app/Models/Illuminate/Enums/ResultRowEnum.php
@@ -11,6 +11,7 @@ enum ResultRowEnum: int
     case ONLY_YEAR = 1;
     case DID_NOT_MATCH_DATE = 2;
     case MULTIPLE_NAMES = 3;
+    case SAME_YEAR_AND_LAST_NAME = 4;
 
     public function trans(): string
     {
@@ -18,7 +19,8 @@ enum ResultRowEnum: int
             self::NOT_SURE->value => 'messages.result_row.not_sure',
             self::ONLY_YEAR->value => 'messages.result_row.only_year',
             self::DID_NOT_MATCH_DATE->value => 'messages.result_row.did_not_match_date',
-            self::MULTIPLE_NAMES->value => 'messages.result_row.multiple_names'
+            self::MULTIPLE_NAMES->value => 'messages.result_row.multiple_names',
+            self::SAME_YEAR_AND_LAST_NAME->value => 'messages.result_row.same_year_and_last_name',
         };
     }
 }
